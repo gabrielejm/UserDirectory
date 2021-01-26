9c4ac95bdabad05f9f2cc8b2c57ae82c5c06260e
Justin Gabriele <gabrielejustin@yahoo.com>
2021-01-26 09:56:00 -0500

    header top done

--- user-directory/src/App.js
+++ user-directory/src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Header />
     </div>
   );
 }
--- user-directory/src/components/Header.js
+++ user-directory/src/components/Header.js
@@ -1,5 +1,18 @@
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="header-top">
+        <div className="header-text">
+          <h1>Employee Directory</h1>
+          <p>Click on carrots to filter by heading or use the search box to narrow your results!</p>
+        </div>
+      </div>
+      <div className="header-bottom"></div>
+    </div>
+  );
 };
+
+export default Header;
